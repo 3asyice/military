8634d4ce8876fb53d334825c1d88de3373e4076d
Easy×͜×Ice <116710976+3asyice@users.noreply.github.com>
2024-08-06 04:09:50 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+var posDiv = document.getElementById('position');
+    
+    var displayPos = false;
+    
+    function positions() {
+      displayPos = !displayPos;
+      posDiv.style.display = displayPos ? 'block' : 'none';
+    }
+    var intDiv = document.getElementById('internatoo');
+    
+    var displayInt = false;
+    
+    function internato() {
+      displayInt = !displayInt;
+      intDiv.style.display = displayInt ? 'block' : 'none';
+    }
+    var insDiv = document.getElementById('inscricoes');
+    
+    var displayIns = false;
+    
+    function inscricao() {
+      displayIns = !displayIns;
+      insDiv.style.display = displayIns ? 'block' : 'none';
+    }
+    
